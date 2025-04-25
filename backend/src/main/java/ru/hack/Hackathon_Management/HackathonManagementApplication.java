942c66ee54bd58499b48033c2e8ed3ee325f4126
iluvatar <kds02122004@mail.ru>
2025-04-25 13:40:09 +0500

    add spring project

---
+++ backend/src/main/java/ru/hack/Hackathon_Management/HackathonManagementApplication.java
@@ -0,0 +1,13 @@
+package ru.hack.Hackathon_Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonManagementApplication.class, args);
+	}
+
+}
